5ef3e66ee561da3fd5780b0e674d41d00aaca932
Rodrigo Villarroel <rodrigo.villarroel.m@gmail.com>
2020-09-25 13:32:29 -0400

    fortune class

---
+++ classes/fortune_definitions.py
@@ -0,0 +1,13 @@
+
+class Fortune:
+    def __init__(self, card_name, type,movement,pay,receive,destination,money):
+        self.card_name = card_name                  # str
+        self.type = type                            # str
+        self.movement= movement                     # bool
+        self.pay=pay                                # bool
+        self.receive=receive                        # bool
+        self.destination=destination                # int
+        self.money=money                            # int
+
+    def operate(self,player):
+        return 0

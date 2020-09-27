a2872bd4bea044f603d49a2517448fc4bb791ae9
Jürgen Ruegenberg Buezo <jurgenruegnberg@gmail.com>
2020-09-27 16:48:13 -0400

    communitychest delete

--- classes/communitychest_definitions.py
+++
@@ -1,9 +0,0 @@
-class CommunityChest:
-    def __init__(self, card_name, type, movement, pay, receive, destination, money):
-        self.card_name = card_name      # str
-        self.type = type                # str
-        self.movement = movement        # bool
-        self.pay = pay                  # bool
-        self.receive = receive          # bool
-        self.destination = destination  # int
-        self.money = money              # int
